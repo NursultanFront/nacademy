14816ccb6d4240a1c86ad029b3871a6328f90034
NursultanFront <nursultan020395@gmail.com>
2023-06-16 01:16:31 +0500

    feat: sort

--- src/pages/HomeView.tsx
+++ src/pages/HomeView.tsx
@@ -1,7 +1,6 @@
-import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
 import { useAppDispatch, useAppSelector } from '../redux-hooks/redux-hooks';
-import { fetchProducts } from '../store/productSlice';
+import { fetchProducts, sortByName, sortByPrice } from '../store/productSlice';
 
 const HomeView = () => {
   const dispatch = useAppDispatch();
@@ -11,11 +10,52 @@ const HomeView = () => {
     dispatch(fetchProducts());
   }, [dispatch]);
 
+  if (product.loading) {
+    <p>Loading</p>;
+  }
+
   return (
     <div>
+      <button
+        onClick={() => {
+          dispatch(sortByName({ order: 'asc' }));
+        }}
+      >
+        по имени вверх
+      </button>
+      <button
+        onClick={() => {
+          dispatch(sortByName({ order: 'desc' }));
+        }}
+      >
+        по имени вниз
+      </button>
+
+      <button
+        onClick={() => {
+          dispatch(sortByPrice({ order: 'desc' }));
+        }}
+      >
+        по Цене вниз
+      </button>
+
+      <button
+        onClick={() => {
+          dispatch(sortByPrice({ order: 'asc' }));
+        }}
+      >
+        по Цене вверх
+      </button>
+
       <ul>
         {product.data.map((item) => {
-          return <li key={item.id}>{item.title}</li>;
+          return (
+            <li key={item.id}>
+              <div>{item.title}</div>
+              <img src={item.thumbnail} alt="" />
+              <div>{item.price}</div>
+            </li>
+          );
         })}
       </ul>
     </div>
